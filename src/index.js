3b19caaa804717f24806928a40b90ad928253838
DevNuaKo175496624 <175496624+devnuako@users.noreply.github.com>
2024-08-14 10:20:50 +0700

    18: Template (View) Engine with ejs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require('express'); //commonjs
+const path = require('path'); //commonjs
 // import express from 'express';//es modules
 
 const app = express();// app express
 const port = 8081; //port
+
+//config template engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
 
 //khai báo route
 app.get('/', (req, res) => {
@@ -14,7 +20,8 @@ app.get('/abc', (req, res) => {
 })
 
 app.get('/xyz', (req, res) => {
-    res.send('<h1> XYZ </h1>')
+    //res.send('<h1> XYZ </h1>')
+    res.render('sample.ejs')
 })
 
 app.listen(port, () => {
