be54f1ad970a9055fddef37dbf6f27677f93fc77
DevNuaKo175496624 <175496624+devnuako@users.noreply.github.com>
2024-08-14 09:58:31 +0700

    17: Hello World with Express and More Routes

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const http = require('http');//node.js
+const express = require('express'); //commonjs
+// import express from 'express';//es modules
 
-const hostname = 'localhost';//localhost
-const port = 6969;
+const app = express();// app express
+const port = 8081; //port
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n from zero');
-});
+//khai báo route
+app.get('/', (req, res) => {
+    res.send('Hello World with Express')
+})
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.get('/abc', (req, res) => {
+    res.send('check ABC')
+})
+
+app.get('/xyz', (req, res) => {
+    res.send('<h1> XYZ </h1>')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
