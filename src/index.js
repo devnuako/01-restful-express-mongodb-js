7ad28b62dabd214606c80029fbb11177f92cf9cd
DevNuaKo175496624 <175496624+devnuako@users.noreply.github.com>
2024-08-14 10:24:31 +0700

    19: Init env with dotenv package

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express'); //commonjs
 const path = require('path'); //commonjs
 // import express from 'express';//es modules
+require('dotenv').config();
+
 
 const app = express();// app express
-const port = 8081; //port
+const port = process.env.PORT || 8888; //port => hardcode . uat .prod
+const hostname = process.env.HOST_NAME;
+
 
 //config template engine
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +28,6 @@ app.get('/xyz', (req, res) => {
     res.render('sample.ejs')
 })
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
